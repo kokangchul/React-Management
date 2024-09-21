65e617607df6fbda5cdcca0a417d1578ab51d8e1
kokangchul <kokangchul@gmail.com>
2024-09-22 01:05:53 +0900

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,7 +1,22 @@
 import React, { useState, useCallback } from "react";
 import axios from "axios";
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+} from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  hidden: {
+    display: "none",
+  },
+}));
 
 const CustomerAdd = ({ stateRefresh }) => {
+  const classes = useStyles();
   const [user, setUser] = useState({
     file: null,
     fileName: "",
@@ -9,7 +24,25 @@ const CustomerAdd = ({ stateRefresh }) => {
     birthday: "",
     gender: "",
     job: "",
+    open: false,
   });
+
+  const handleClickOpen = () => {
+    setUser((prev) => ({ ...prev, open: true }));
+  };
+
+  const handleClickClose = () => {
+    setUser({
+      file: null,
+      fileName: "",
+      name: "",
+      birthday: "",
+      gender: "",
+      job: "",
+      open: false,
+    });
+  };
+
   const addCustomer = useCallback(() => {
     const url = "/api/customers";
     const formData = new FormData();
@@ -39,6 +72,7 @@ const CustomerAdd = ({ stateRefresh }) => {
           birthday: "",
           gender: "",
           job: "",
+          open: false,
         });
       });
     },
@@ -58,45 +92,73 @@ const CustomerAdd = ({ stateRefresh }) => {
   }, []);
 
   return (
-    <form onSubmit={onSubmit}>
-      <h1>고객추가</h1>
-      프로필 이미지:
-      <input type="file" name="file" onChange={handleFileChange} />
-      <br />
-      이름:
-      <input
-        type="text"
-        name="name"
-        value={user.name}
-        onChange={handleValueChange}
-      />
-      <br />
-      생년월일:
-      <input
-        type="text"
-        name="birthday"
-        value={user.birthday}
-        onChange={handleValueChange}
-      />
-      <br />
-      성별:
-      <input
-        type="text"
-        name="gender"
-        value={user.gender}
-        onChange={handleValueChange}
-      />
-      <br />
-      직업:
-      <input
-        type="text"
-        name="job"
-        value={user.job}
-        onChange={handleValueChange}
-      />
-      <br />
-      <button type="submit">추가하기</button>
-    </form>
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        고객 추가하기
+      </Button>
+      <Dialog open={user.open} onClose={handleClickClose}>
+        <DialogTitle>고객 추가</DialogTitle>
+        <DialogContent>
+          <input
+            className={classes.hidden}
+            accept="image/*"
+            id="raised-button-file"
+            type="file"
+            onChange={handleFileChange}
+          />
+          <label htmlFor="raised-button-file">
+            <Button
+              variant="contained"
+              color="primary"
+              component="span"
+              name="file"
+            >
+              {user.fileName === "" ? "프로필 이미지 선택" : user.fileName}
+            </Button>
+          </label>
+          <br />
+          <TextField
+            label="이름"
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={handleValueChange}
+          />
+          <br />
+          <TextField
+            label="생년월일"
+            type="text"
+            name="birthday"
+            value={user.birthday}
+            onChange={handleValueChange}
+          />
+          <br />
+          <TextField
+            label="성별"
+            type="text"
+            name="gender"
+            value={user.gender}
+            onChange={handleValueChange}
+          />
+          <br />
+          <TextField
+            label="직업"
+            type="text"
+            name="job"
+            value={user.job}
+            onChange={handleValueChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="primary" onClick={onSubmit}>
+            추가
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleClickClose}>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,20 +1,54 @@
 import axios from "axios";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+} from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 
 const CustomerDelete = ({ id, stateRefresh }) => {
+  const [open, setOpen] = useState(false);
+
   function deleteCustomer(id) {
     const url = "/api/customers/" + id;
     axios.delete(url);
     stateRefresh();
   }
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClickClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <button
-      onClick={(e) => {
-        deleteCustomer(id);
-      }}
-    >
-      삭제
-    </button>
+    <div>
+      <Button variant="contained" color="secondary" onClick={handleClickOpen}>
+        삭제
+      </Button>
+      <Dialog open={open}>
+        <DialogTitle onClose={handleClickClose}>삭제 경고</DialogTitle>
+        <DialogContent>
+          <Typography gutterBottom>선택한 고객 정보가 삭제됩니다.</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={(e) => deleteCustomer(id)}
+          >
+            삭제
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleClickClose}>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
